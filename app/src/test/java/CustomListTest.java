148002614e406cee9bb6993b753759454168967f
LeoLrw <leolrw1@gmail.com>
2022-03-10 22:19:45 -0700

    Initial Test

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import com.example.simpleparadox.listycity.CustomList;
+import com.example.simpleparadox.listycity.City;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomListTest {
+    private CustomList list;
+
+    @Before
+    public void createList() {
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(list.getCount(), listSize+1);
+    }
+}
+
+
