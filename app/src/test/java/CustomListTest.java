2a06ffcd6890f0a4e33cacb2ed0c6e18c539b8fb
LeoLrw <leolrw1@gmail.com>
2022-03-11 15:57:03 -0700

    Added hasCityTest

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import com.example.simpleparadox.listycity.CustomList;
 import com.example.simpleparadox.listycity.City;
@@ -20,6 +22,17 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Halifax", "NS"));
         assertEquals(list.getCount(), listSize+1);
+    }
+
+    @Test
+    void hasCityTest() {
+        createList();
+
+        City cityTest = new City("Charlottetown", "PEI");
+        assertFalse(list.hasCity(cityTest));
+
+        list.add(cityTest);
+        assertTrue(list.hasCity(cityTest));
     }
 }
 
